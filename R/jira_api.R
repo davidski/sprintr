b1ab8a12d959a0020d94700f20fd0490f77931ca
Susana Quartin <susana.quartin@talkdesk.com>
2018-09-29 21:40:09 +0100

    Get boards until page returned is last

--- R/jira_api.R
+++ R/jira_api.R
@@ -106,17 +106,36 @@ get_fields <- function() {
 #' @return dataframe
 #' @export
 #'
-#' @importFrom dplyr bind_cols select
+#' @importFrom dplyr bind_cols bind_rows data_frame select
 #' @importFrom purrr pluck
 #' @importFrom rlang .data
+#' @importFrom glue glue
 #'
 #' @examples
 #' NULL
 get_boards <- function() {
-  all_boards <- jira_api("/rest/agile/1.0/board") %>%
-    purrr::pluck("content", "values")
-  dplyr::bind_cols(dplyr::select(all_boards, -.data$location),
-                   purrr::pluck(all_boards, "location"))
+  content <- list(isLast = FALSE,
+                  startAt = 0,
+                  maxResults = 0)
+
+  all_boards <- dplyr::data_frame()
+
+  while (!content$isLast) {
+    start_at <- content$startAt + content$maxResults
+    url <- glue::glue("/rest/agile/1.0/board/?",
+                      "startAt={start_at}")
+
+    content <- jira_api(url) %>%
+      purrr::pluck("content")
+
+    values <- purrr::pluck(content, "values")
+
+    boards <- dplyr::bind_cols(dplyr::select(values, -.data$location),
+                                   purrr::pluck(values, "location"))
+
+    all_boards <- dplyr::bind_rows(all_boards, boards)
+  }
+  all_boards
 }
 
 #' Title
